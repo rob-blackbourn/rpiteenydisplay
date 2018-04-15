db9fb130130f2f0fd254b70ccd4d8c8d19d35945
Rob Blackboun <rob.blackbourn@gmail.com>
2018-04-15 15:24:46 +0100

    Added a signal stength style graph

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from luma.oled.device import ssd1306
 from PIL import ImageFont
 
 from rpimonitor.monitor.stat import sample_proc_stat
-from rpimonitor.display.cpu_detail import calc_and_draw_cpu_detail_text
+from rpimonitor.display.cpu_detail import calc_and_draw_cpu_detail_text, draw_signal_strength_graph
 
 def monitor_cpu(device, font, count=100):
     """Monitor the cpu"""
@@ -28,12 +28,41 @@ def monitor_cpu(device, font, count=100):
 
         i = i + 1
 
+def monitor_cpu2(device, count=100):
+    """Monitor the cpu"""
+    stats = sample_proc_stat()
+    cpu, cores = stats['cpu'], stats['cores']
+    i = 0
+    while i < count:
+
+        time.sleep(1)
+
+        prev_cpu, prev_cores = cpu, cores
+
+        stats = sample_proc_stat()
+        cpu, cores = stats['cpu'], stats['cores']
+        draw_signal_strength_graph(device, (5,5), (cpu - prev_cpu).usage)
+
+        i = i + 1
+
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, width=128, height=32)
 
 font = ImageFont.truetype('DejaVuSans.ttf', 12)
 
-monitor_cpu(device, font)
+draw_signal_strength_graph(device, (0, 0), 0.0, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.1, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.2, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.3, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.4, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.5, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.6, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.7, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.8, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 0.9, 5, (4,4))
+draw_signal_strength_graph(device, (0, 0), 1.0, 5, (4,4))
+#monitor_cpu(device, font)
+monitor_cpu2(device)
 
 #sys.stdout.write("Press ENTER to continue\n")
 #sys.stdin.readline()
--- rpimonitor/display/cpu_detail.py
+++ rpimonitor/display/cpu_detail.py
@@ -28,7 +28,6 @@ def draw_cpu_detail_text(device, font, total, cores):
         x0, x1, x2 = 0, 50, 90
         y0, y1 = 0, 20
 
-
         draw.text((x0 + cpu_pad, y0), "CPU", fill="white", font=font)
         draw.text((x0 + total_pad, y1), total_text, fill="white", font=font)
         draw.text((x1 + core0_pad, y0), core0_text, fill="white", font=font)
@@ -43,3 +42,45 @@ def calc_and_draw_cpu_detail_text(device, font, prev_cpu, cpu, prev_cores, cores
     for key in cores.keys():
         cores_delta.append((cores[key] - prev_cores[key]).usage)
     draw_cpu_detail_text(device, font, cpu_delta, cores_delta)
+
+def offset_rectangle(location, rectangle):
+    return (location[0] + rectangle[0], location[1] + rectangle[1], location[0] + rectangle[2], location[1] + rectangle[3])
+
+def draw_signal_strength_graph(device, location, value, bar_count=5, unit_size=(2,2)):
+    """Draw a signal stregth graph where value varies from 0 to 1"""
+    border_thickness = 1
+    padding = 1
+    total_height = unit_size[1] * bar_count + 2 * border_thickness
+    total_width = (unit_size[0] + 2 * border_thickness) * bar_count + (bar_count - 1) * padding
+    with canvas(device) as draw:
+        for bar in range(bar_count):
+            min_bar_value, max_bar_value = bar / bar_count, (bar + 1) / bar_count
+            if value > max_bar_value:
+                bar_value = 1
+            elif value < min_bar_value:
+                bar_value = 0
+            else:
+                bar_value = (value - min_bar_value) * bar_count
+
+            total_bar_height = (bar + 1) * unit_size[1] + 2 * border_thickness
+            left = bar * (2 * border_thickness + unit_size[0]) + bar * padding
+            right = left + 2 * border_thickness + unit_size[0]
+            bar_top = total_height - total_bar_height
+            bar_rectangle = (left, bar_top, right, total_height)
+            if bar_value == 0:
+                r = offset_rectangle(location, bar_rectangle)
+                draw.rectangle(r, fill='black', outline='white')                
+            elif bar_value == 1:
+                r = offset_rectangle(location, bar_rectangle)
+                draw.rectangle(r, fill='white', outline='white')
+            else:
+                r = offset_rectangle(location, bar_rectangle)
+                draw.rectangle(r, fill='black', outline='white')
+                filled_bar_height = int(round(bar_value * (bar + 1) * unit_size[1]))
+                filled_top = total_height - (2 * border_thickness + filled_bar_height)
+                filled_rectangle = (left, filled_top, right, total_height)
+                r = offset_rectangle(location, filled_rectangle)
+                draw.rectangle(r, fill='white', outline=None)
+
+
+
