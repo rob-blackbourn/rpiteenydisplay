25498c4335a80a6a5db0a2cbe7b089ac5173949f
Rob Blackboun <rob.blackbourn@gmail.com>
2018-04-21 13:05:28 +0100

    Got meminfo working

--- rpimonitor/monitor/meminfo.py
+++ rpimonitor/monitor/meminfo.py
@@ -1,36 +1,118 @@
 """Process /proc/meminfo """
 
-def _parse_proc_meminfo_line(line):
-    name, value, unit = line.split()
-    name, value, unit = name[:-1], int(value), unit.lower()
-    if unit == "kb":
-        value *= 1024
-    elif unit == "mb":
-        value *= 1024 * 1024
-    elif unit == "gb":
-        value *= 1024 * 1024 * 1024
-    return name, value
+class MemInfo(object):
+    """Methods for interacting with /proc/meminfo"""
 
-def parse_proc_meminfo_lines(lines):
-    """Parse the lines returned from /proc/meminfo"""
-    results = {}
-    for line in lines:
-        name, value = _parse_proc_meminfo_line(line)
-        results[name] = value
-    return results
+    def __init__(self, values):
+        self.values = values
 
-def parse_proc_meminfo_lines2(lines):
-    """Parse the lines returned from /proc/meminfo"""
-    return dict(_parse_proc_meminfo_line(line) for line in lines)
+    @classmethod
+    def _parse_line(cls, line):
+        name, value, _ = line.split()
+        return name[:-1], int(value)
 
-def sample_proc_meminfo():
-    """Takes a reading from /proc/stat and returns the parsed data"""
-    with open('/proc/meminfo') as f:
-        return parse_proc_meminfo_lines2(f.readlines())
+    @classmethod
+    def parse_lines(cls, lines):
+        """Parse the lines returned from /proc/meminfo"""
+        return MemInfo(dict(cls._parse_line(line) for line in lines))
+
+    @classmethod
+    def sample(cls):
+        """Takes a reading from /proc/stat and returns the parsed data"""
+        with open('/proc/meminfo') as f:
+            return cls.parse_lines(f.readlines())
+
+    @property
+    def total(self):
+        """Total installed memory (MemTotal and SwapTotal in
+              /proc/meminfo)"""
+        return self.mem_total + self.swap_total
+
+    @property
+    def mem_total(self):
+        """Total physical memory (MemTotal in /proc/meminfo)"""
+        return self.values['MemTotal']
+
+    @property
+    def swap_total(self):
+        """Total swap memory (SwapTotal in /proc/meminfo)"""
+        return self.values['SwapTotal']
+        
+    @property
+    def used(self):
+        """Used memory (calculated as total - free - buffers - cache)"""
+        return self.total - self.free - self.buffers - self.cache
+        
+    @property
+    def mem_used(self):
+        """Used physical memory (calculated as mem_total - mem_free - buffers - cache)"""
+        return self.mem_total - self.mem_free - self.buffers - self.cache
+        
+    @property
+    def swap_used(self):
+        """Used swap (calculated as total - free - buffers - cache)"""
+        return self.swap_total - self.swap_free
+
+    @property
+    def free(self):
+        """Unused memory (MemFree and SwapFree in /proc/meminfo)"""
+        return self.values['MemFree'] + self.values['SwapFree']
+
+    @property
+    def mem_free(self):
+        """Unused physical memory (MemFree in /proc/meminfo)"""
+        return self.values['MemFree']
+
+    @property
+    def swap_free(self):
+        """Unused swap memory (SwapFree in /proc/meminfo)"""
+        return self.values['SwapFree']
+
+    @property
+    def shared(self):
+        """Memory used (mostly) by tmpfs (Shmem in /proc/meminfo)"""
+        return self.values['Shmem']
+
+    @property
+    def buffers(self):
+        """Memory used by kernel buffers (Buffers in /proc/meminfo)"""
+        return self.values['Buffers']
+
+    @property
+    def cache(self):
+        """Memory used by the page cache and slabs (Cached and SReclaimable
+           in /proc/meminfo)"""
+        return self.values['Cached'] + self.values['SReclaimable']
+
+    @property
+    def available(self):
+        """Estimation of how much memory is available for starting new, 
+           without swapping (MemAvailable in /proc/meminfo)."""
+        return self.values['MemAvailable']
+
+    @property
+    def usage(self):
+        """Percentyage of all memory used"""
+        return self.used / self.total
+
+    @property
+    def mem_usage(self):
+        """Percentyage of all memory used"""
+        return self.mem_used / self.mem_total
+
+    @property
+    def swap_usage(self):
+        """Percentyage of all memory used"""
+        return self.swap_used / self.swap_total
+
+    def __str__(self):
+        return f"total: {meminfo.total}, used: {meminfo.used}, free: {meminfo.free}, shared: {self.shared}, buffers: {self.buffers}, cache: {self.cache}, buff/cache: {self.buffers + self.cache} available: {meminfo.available}"
 
 if __name__ == "__main__":
-    meminfo = sample_proc_meminfo()
+    meminfo = MemInfo.sample()
     print(meminfo)
-    print(meminfo['MemTotal'] / (1024 * 1024))
+    print(meminfo.usage)
+    print(meminfo.mem_usage)
+    print(meminfo.swap_usage)
     
     
