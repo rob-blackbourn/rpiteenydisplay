350bea43b1169a9721ad11252a01552ff8f1b32e
Rob Blackboun <rob.blackbourn@gmail.com>
2018-04-21 13:24:11 +0100

    Classified

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-import sys
-
-print(sys.path)
-
 import time
 from luma.core.interface.serial import i2c, spi
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
 from PIL import ImageFont
 
-from rpimonitor.monitor.stat import sample_proc_stat
+from rpimonitor.monitor.stat import Stat
 from rpimonitor.display.cpu_detail import calc_and_draw_cpu_detail_text, draw_signal_strength_graph
 
 def monitor_cpu(device, font, count=100):
     """Monitor the cpu"""
-    stats = sample_proc_stat()
-    cpu, cores = stats['cpu'], stats['cores']
+    stats = Stat.sample()
+    cpu, cores = stats.cpu, stats.cores
     i = 0
     while i < count:
 
@@ -22,16 +18,16 @@ def monitor_cpu(device, font, count=100):
 
         prev_cpu, prev_cores = cpu, cores
 
-        stats = sample_proc_stat()
-        cpu, cores = stats['cpu'], stats['cores']
+        stats = Stat.sample()
+        cpu, cores = stats.cpu, stats.cores
         calc_and_draw_cpu_detail_text(device, font, prev_cpu, cpu, prev_cores, cores)
 
         i = i + 1
 
 def monitor_cpu2(device, count=100):
     """Monitor the cpu"""
-    stats = sample_proc_stat()
-    cpu, cores = stats['cpu'], stats['cores']
+    stats = Stat.sample()
+    cpu, cores = stats.cpu, stats.cores
     i = 0
     while i < count:
 
@@ -39,8 +35,8 @@ def monitor_cpu2(device, count=100):
 
         prev_cpu, prev_cores = cpu, cores
 
-        stats = sample_proc_stat()
-        cpu, cores = stats['cpu'], stats['cores']
+        stats = Stat.sample()
+        cpu, cores = stats.cpu, stats.cores
         draw_signal_strength_graph(device, (5,5), (cpu - prev_cpu).usage)
 
         i = i + 1
@@ -50,19 +46,19 @@ device = ssd1306(serial, width=128, height=32)
 
 font = ImageFont.truetype('DejaVuSans.ttf', 12)
 
-draw_signal_strength_graph(device, (0, 0), 0.0, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.1, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.2, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.3, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.4, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.5, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.6, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.7, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.8, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 0.9, 5, (4,4))
-draw_signal_strength_graph(device, (0, 0), 1.0, 5, (4,4))
-#monitor_cpu(device, font)
-monitor_cpu2(device)
+#draw_signal_strength_graph(device, (0, 0), 0.0, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.1, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.2, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.3, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.4, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.5, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.6, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.7, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.8, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 0.9, 5, (4,4))
+#draw_signal_strength_graph(device, (0, 0), 1.0, 5, (4,4))
+monitor_cpu(device, font)
+#monitor_cpu2(device)
 
 #sys.stdout.write("Press ENTER to continue\n")
 #sys.stdin.readline()
--- rpimonitor/monitor/stat.py
+++ rpimonitor/monitor/stat.py
@@ -1,7 +1,7 @@
 """Routines to get information from /proc/stat on a Raspberry Pi 3 B+ running Raspbian Stretch
 """
 
-class CpuStat(object):
+class Cpu(object):
     """ Represents the cpu data from /proc/stat
     """
 
@@ -34,7 +34,7 @@ class CpuStat(object):
 
     def __sub__(self, other):
         """Calculate the change between two readings"""
-        return CpuStat(
+        return Cpu(
             self.user - other.user,
             self.nice - other.nice,
             self.system - other.system,
@@ -46,27 +46,50 @@ class CpuStat(object):
             self.quest - other.quest,
             self.quest_nice - other.quest_nice)
 
-def parse_proc_stat_lines(lines):
-    """Parses the lines of /proc/stat"""
-    stats = {}
-    for line in map(lambda x: x.strip(), lines):
-        items = line.split(' ')
-        name, values = items[0], items[1:]
-        if name.startswith('cpu'):
-            if name == "cpu":
-                stats[name] = CpuStat(*list(map(int, values[1:])))
-            else:
-                if 'cores' not in stats:
-                    stats['cores'] = {}
-                cpu_id = int(name[3:])
-                stats['cores'][cpu_id] = CpuStat(*list(map(int, values)))
-        elif name in ['ctxt', 'btime', 'processes', 'procs_running', 'procs_blocked']:
-            stats[name] = int(values[0])
-        else:
-            stats[name] = list(map(int, values))
-    return stats
+    def __str__(self):
+        return f"busy_time: {self.busy_time}, idle_time: {self.idle_time}, usage: {self.usage:.2%}"
 
-def sample_proc_stat():
-    """Takes a reading from /proc/stat and returns the parsed data"""
-    with open('/proc/stat') as fp:
-        return parse_proc_stat_lines(fp.readlines())
+class Stat(object):
+    """A class for /proc/stat"""
+
+    def __init__(self, **kwargs):
+        self.cpu = kwargs['cpu']
+        self.cores = kwargs['cores']
+        self.intr = kwargs['intr']
+        self.ctxt = kwargs['ctxt']
+        self.btime = kwargs['btime']
+        self.processes = kwargs['processes']
+        self.procs_running = kwargs['procs_running']
+        self.procs_blocked = kwargs['procs_blocked']
+        self.softirq = kwargs['softirq']
+
+    @classmethod
+    def parse(cls, lines):
+        """Parses the lines of /proc/stat"""
+        stats = {}
+        for line in map(lambda x: x.strip(), lines):
+            items = line.split(' ')
+            name, values = items[0], items[1:]
+            if name.startswith('cpu'):
+                if name == "cpu":
+                    stats[name] = Cpu(*list(map(int, values[1:])))
+                else:
+                    if 'cores' not in stats:
+                        stats['cores'] = {}
+                    cpu_id = int(name[3:])
+                    stats['cores'][cpu_id] = Cpu(*list(map(int, values)))
+            elif name in ['ctxt', 'btime', 'processes', 'procs_running', 'procs_blocked']:
+                stats[name] = int(values[0])
+            else:
+                stats[name] = list(map(int, values))
+        return Stat(**stats)
+
+    @classmethod
+    def sample(cls):
+        """Takes a reading from /proc/stat and returns the parsed data"""
+        with open('/proc/stat') as fp:
+            return cls.parse(fp.readlines())
+
+if __name__ == "__main__":
+    stat = Stat.sample()
+    print(str(stat.cpu))
