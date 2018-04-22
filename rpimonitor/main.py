206f5e554eacb56f78e59c7cbccae9735121e919
Rob Blackboun <rob.blackbourn@gmail.com>
2018-04-22 13:06:22 +0100

    Got the web page updating with cpus

--- rpimonitor/main.py
+++ rpimonitor/main.py
@@ -6,16 +6,9 @@ from aiohttp import web
 import jinja2
 import aiohttp_jinja2
 
-from rpimonitor.views.monitor import Monitor
+from rpimonitor.views.monitor import Monitor, index, index_ws
 
 PROJECT_ROOT = os.path.dirname(__file__)
-
-
-
-@aiohttp_jinja2.template('index.html')
-async def index(request):
-    """The index page"""
-    return {}
 
 def main(args):
     """Run the web server"""
@@ -23,5 +16,6 @@ def main(args):
     app.router.add_static('/node_modules/', path=os.path.join(PROJECT_ROOT, 'node_modules'), name='node_modules')
     aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('rpimonitor', 'templates'))
     app.router.add_get('/', index)
-    app['system_monitor'] = Monitor(app)
+    app.router.add_get('/ws', index_ws)
+    app['monitor'] = Monitor(app)
     web.run_app(app, host='127.0.0.1', port=8080)
--- rpimonitor/views/monitor.py
+++ rpimonitor/views/monitor.py
@@ -1,15 +1,15 @@
 """The main monitor class"""
 
 import asyncio
-import time
+from aiohttp import web
+import aiohttp_jinja2
 
-from luma.core.interface.serial import i2c, spi
-from luma.core.render import canvas
+from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from PIL import ImageFont
 
 from rpimonitor.monitor.stat import Stat
-from rpimonitor.display.cpu_detail import calc_and_draw_cpu_detail_text
+from rpimonitor.display.cpu_detail import draw_cpu_detail_text
 
 class Monitor(object):
     """Integrates with aiohttp to provide a polling background monitor"""
@@ -18,8 +18,9 @@ class Monitor(object):
         self.is_monitoring = True
         self.poll_seconds = poll_seconds
         self.background_task = None
-        self.stats = None
-        self.prev_stats = None
+        self.stat = None
+        self.prev_stat = None
+        self.condition = asyncio.Condition()
         serial = i2c(port=1, address=0x3C)
         self.device = ssd1306(serial, width=128, height=32)
         self.font = ImageFont.truetype('DejaVuSans.ttf', 12)        
@@ -28,25 +29,26 @@ class Monitor(object):
 
     def render(self):
         """Render the status"""
-        calc_and_draw_cpu_detail_text(
-            self.device, self.font, 
-            self.prev_stats.cpu, self.stats.cpu,
-            self.prev_stats.cores, self.stats.cores)
+        draw_cpu_detail_text(self.device, self.font, self.cpu_usage, self.core_usages)
         
     async def poll(self):
         """Poll the status"""
-        self.stats = await Stat.sample_async()
+        self.stat = await Stat.sample_async()
         try:
             while self.is_monitoring:
                 print("Monitoring")
                 await asyncio.sleep(self.poll_seconds)
 
-                self.prev_stats = self.stats
-                self.stats = await Stat.sample_async()
+                self.prev_stat = self.stat
+                self.stat = await Stat.sample_async()
+                with await self.condition:
+                    self.condition.notify_all()
                 self.render()
 
         except asyncio.CancelledError:
             pass
+        except Exception as error:
+            print(error)
         
     async def on_startup(self, app):
         """Start monitoring the system state"""
@@ -60,3 +62,38 @@ class Monitor(object):
             self.background_task.cancel()
         except Exception as error:
             print(error)
+
+    @property
+    def cpu_usage(self):
+        return (self.stat.cpu - self.prev_stat.cpu).usage
+
+    @property
+    def core_usages(self):
+        return [(self.stat.cores[key] - self.prev_stat.cores[key]).usage for key in self.stat.cores.keys()]
+
+@aiohttp_jinja2.template('index.html')
+async def index(request):
+    """The index page"""
+    return {}
+
+async def index_ws(request):
+    """The index page"""
+    monitor = request.app['monitor']
+
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    while not ws.closed:
+        print("Acquiring")
+        with await monitor.condition:
+            print("Waiting")
+            await monitor.condition.wait()
+            print("Received")
+            cpu_usage = 100 * monitor.cpu_usage
+            core_usages = [100 * usage for usage in monitor.core_usages]
+            data = {'cpu_usage': cpu_usage, 'core_usages': core_usages}
+            await ws.send_json(data)
+
+    print('websocket connection closed')
+
+    return ws
