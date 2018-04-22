0cbc92fa419fa725ed0737b248dda586dc8dc23b
Rob Blackboun <rob.blackbourn@gmail.com>
2018-04-22 06:35:24 +0100

    Turned into a class

--- rpimonitor/monitor/thermal.py
+++ rpimonitor/monitor/thermal.py
@@ -2,19 +2,26 @@
 
 import os
 
-def get_cpu_temp():
-    """ Return CPU temperature """
-    with open("/sys/class/thermal/thermal_zone0/temp") as f:
-        return float(f.readline()) / 1000
+class Temp(object):
+    """Class for reading /sys/class/thermal/thermal_zone0/temp"""
 
+    def __init__(self, temp):
+        self.temp = temp
 
-def get_gpu_temp():
-    """ Return GPU temperature as a character string"""
-    res = os.popen('/opt/vc/bin/vcgencmd measure_temp').readline()
-    return float(res.replace("temp=", "").replace("'C\n", ""))
+    @classmethod
+    def _parse(cls, line):
+        """Parse the temperature"""
+        return Temp(float(line) / 1000)
 
+    @classmethod
+    def sample(cls):
+        """Sample CPU temperature"""
+        with open("/sys/class/thermal/thermal_zone0/temp") as f:
+            return cls._parse(f.readline())
+
+    def __str__(self):
+        return f"temp: {self.temp}"
+    
 if __name__ == "__main__":
-    temp = get_cpu_temp()
-    print(temp)
-    temp2 = get_gpu_temp()
-    print(temp2)
+    temp = Temp.sample()
+    print(str(temp))
